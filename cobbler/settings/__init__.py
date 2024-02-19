57065366cda71929922a133b98fed460aeaab891
Marek Czernek <marek.czernek@suse.com>
2024-02-19 10:18:52 +0100

    Provide option to use pre-built GRUB bootloader (#81)
    When using secure boot, users must use a signed
    GRUB bootloader that is loaded by the shim file.
    This change enables users to specify the location
    and name of a signed GRUB bootloader just as we
    do with the shim.efi file.
    
    This is a backport of cobbler#3570.

--- cobbler/settings/__init__.py
+++ cobbler/settings/__init__.py
@@ -1,6 +1,7 @@
 """
 Cobbler app-wide settings
 """
+
 # SPDX-License-Identifier: GPL-2.0-or-later
 # SPDX-FileCopyrightText: Copyright 2006-2008, Red Hat, Inc and Others
 # SPDX-FileCopyrightText: Michael DeHaan <michael.dehaan AT gmail>
@@ -66,6 +67,8 @@ class Settings:
         self.bootloaders_dir = "/var/lib/cobbler/loaders"
         self.bootloaders_shim_folder = "/usr/share/efi/*/"
         self.bootloaders_shim_file = r"shim\.efi$"
+        self.secure_boot_grub_folder = "/usr/share/efi/*/"
+        self.secure_boot_grub_file = r"grub\.efi$"
         self.bootloaders_ipxe_folder = "/usr/share/ipxe/"
         self.grubconfig_dir = "/var/lib/cobbler/grub_config"
         self.build_reporting_enabled = False
